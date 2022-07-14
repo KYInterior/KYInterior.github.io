12c8e56155fde894d13430a11f1a7093b3a9f056
KYInterior <kyinterior.in@gmail.com>
2022-07-14 20:21:19 +0530

    Big Update
    New UI and feel
    More Information
    New Image

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+  const hamburger_menu = document.querySelector(".hamburger-menu");
+  const nav = document.querySelector("nav");
+  const item = document.querySelector(".item-1");
+  const item_2 = document.querySelector(".item-2");
+  const item_3 = document.querySelector(".item-3");
+  const item_4 = document.querySelector(".item-4");
+  const item_5 = document.querySelector(".item-5");
+
+  hamburger_menu.addEventListener("click", () =>{
+    nav.classList.toggle("active");
+    hamburger_menu.classList.toggle("active");
+    item.classList.toggle("active");
+    item_2.classList.toggle("active");
+    item_3.classList.toggle("active");
+    item_4.classList.toggle("active");
+    item_5.classList.toggle("active");
+  })
+
+function sendEmail() {
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "sutharkavya240@gmail.com",
+    Password : "KAVYA@123",
+    To : 'kyinterior.in@gmail.com',
+    From : "sutharkavya240@gmail.com",
+    Subject : "This is the subject",
+    Body : "Hi!"
+      }).then(
+        message => alert(message)
+      );
+}
